9e7e461e1d0a16fe41ee6f6456d7a549a15cb385
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-24 03:21:00 +0100

    Add code to find the number of grains at a given square, and also the total on the board

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+    if s == 1 {
+        1
+    } else {
+        let mut counter = 0;
+        let mut result = 1;
+        loop {
+            if counter < s - 1 {
+                result *= 2;
+                counter += 1;
+            } else {
+                break;
+            }
+        }
+        result
+    }
 }
 
 pub fn total() -> u64 {
-    todo!();
+    let mut total_grains: u64 = 0;
+    let mut counter = 1;
+    loop {
+        if counter <= 64 {
+            total_grains += square(counter);
+            counter += 1;
+        } else {
+            break;
+        }
+    }
+    total_grains
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,4 @@
-fn main() {}
+fn main() {
+    let total = grains::total();
+    println!("Total {total}")
+}
